d87afb9107348299296a8aec24a57c452512d8be
Joachim Metz <joachim.metz@gmail.com>
2016-12-29 18:55:21 +0100

    Code review: 318180043: Added time elements in milliseconds support

--- dfdatetime/time_elements.py
+++ dfdatetime/time_elements.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 """Time elements implementation."""
 
+import calendar
+
 from dfdatetime import definitions
 from dfdatetime import interface
 
@@ -24,15 +26,19 @@ class TimeElements(interface.DateTimeValues):
       time_elements_tuple (Optional[tuple[int, int, int, int, int, int]]):
           time elements, contains year, month, day of month, hours, minutes and
           seconds.
+
+    Raises:
+      ValueError: if the time elements tuple is invalid.
     """
     super(TimeElements, self).__init__()
-    if time_elements_tuple is None:
-      self._number_of_seconds = None
-    else:
-      self._number_of_seconds = self._GetNumberOfSecondsFromElements(
-          *time_elements_tuple)
+    self._number_of_seconds = None
     self._time_elements_tuple = time_elements_tuple
     self.precision = definitions.PRECISION_1_SECOND
+
+    if time_elements_tuple:
+      # Using calendar.timegm here since we want it to raise ValueError
+      # on error.
+      self._number_of_seconds = calendar.timegm(time_elements_tuple)
 
   def CopyFromString(self, time_string):
     """Copies time elements from a string containing a date and time value.
@@ -125,3 +131,91 @@ class TimeElements(interface.DateTimeValues):
     if self._number_of_seconds is None:
       return
     return self._number_of_seconds * 1000000
+
+
+class TimeElementsInMilliseconds(TimeElements):
+  """Class that implements time elements in milliseconds.
+
+  Attributes:
+    is_local_time (bool): True if the date and time value is in local time.
+    precision (str): precision of the date and time value, which should
+        be one of the PRECISION_VALUES in definitions.
+  """
+
+  def __init__(self, time_elements_tuple=None):
+    """Initializes time elements.
+
+    Args:
+      time_elements_tuple (Optional[tuple[int, int, int, int, int, int, int]]):
+          time elements, contains year, month, day of month, hours, minutes,
+          seconds and milliseconds.
+
+    Raises:
+      ValueError: if the time elements tuple is invalid.
+    """
+    milliseconds = None
+    if time_elements_tuple:
+      milliseconds = time_elements_tuple[6]
+      time_elements_tuple = time_elements_tuple[:6]
+
+      if milliseconds < 0 or milliseconds > 999:
+        raise ValueError(u'Invalid number of milliseconds.')
+
+    super(TimeElementsInMilliseconds, self).__init__(
+        time_elements_tuple=time_elements_tuple)
+    self._milliseconds = milliseconds
+    self.precision = definitions.PRECISION_1_MILLISECOND
+
+  def CopyFromString(self, time_string):
+    """Copies time elements from a string containing a date and time value.
+
+    Args:
+      time_string (str): date and time value formatted as:
+          YYYY-MM-DD hh:mm:ss.######[+-]##:##
+
+          Where # are numeric digits ranging from 0 to 9 and the seconds
+          fraction can be either 3 or 6 digits. The time of day, seconds
+          fraction and time zone offset are optional. The default time zone
+          is UTC.
+    """
+    date_time_values = self._CopyDateTimeFromString(time_string)
+
+    year = date_time_values.get(u'year', 0)
+    month = date_time_values.get(u'month', 0)
+    day_of_month = date_time_values.get(u'day_of_month', 0)
+    hours = date_time_values.get(u'hours', 0)
+    minutes = date_time_values.get(u'minutes', 0)
+    seconds = date_time_values.get(u'seconds', 0)
+    microseconds = date_time_values.get(u'microseconds', 0)
+    milliseconds, _ = divmod(microseconds, 1000)
+
+    self._time_elements_tuple = (
+        year, month, day_of_month, hours, minutes, seconds, milliseconds)
+    self._number_of_seconds = self._GetNumberOfSecondsFromElements(
+        year, month, day_of_month, hours, minutes, seconds)
+    self._milliseconds = milliseconds
+
+    self.is_local_time = False
+
+  def CopyToStatTimeTuple(self):
+    """Copies the time elements to a stat timestamp tuple.
+
+    Returns:
+      tuple[int, int]: a POSIX timestamp in seconds and the remainder in
+          100 nano seconds or (None, None) on error.
+    """
+    if self._number_of_seconds is None or self._milliseconds is None:
+      return None, None
+
+    return self._number_of_seconds, self._milliseconds * 1000
+
+  def GetPlasoTimestamp(self):
+    """Retrieves a timestamp that is compatible with plaso.
+
+    Returns:
+      int: a POSIX timestamp in microseconds or None on error.
+    """
+    if self._number_of_seconds is None or self._milliseconds is None:
+      return
+
+    return ((self._number_of_seconds * 1000) + self._milliseconds) * 1000
--- tests/time_elements.py
+++ tests/time_elements.py
@@ -7,10 +7,23 @@ import unittest
 from dfdatetime import time_elements
 
 
-class TimeElementsTimeTest(unittest.TestCase):
+class TimeElementsTest(unittest.TestCase):
   """Tests for the time elements."""
 
   # pylint: disable=protected-access
+
+  def testInitialize(self):
+    """Tests the initialization function."""
+    time_elements_object = time_elements.TimeElements()
+    self.assertIsNotNone(time_elements_object)
+
+    time_elements_object = time_elements.TimeElements(
+        time_elements_tuple=(2010, 8, 12, 20, 6, 31))
+    self.assertIsNotNone(time_elements_object)
+
+    with self.assertRaises(ValueError):
+      time_elements.TimeElements(
+          time_elements_tuple=(2010, 13, 12, 20, 6, 31))
 
   def testCopyFromString(self):
     """Tests the CopyFromString function."""
@@ -175,6 +188,196 @@ class TimeElementsTimeTest(unittest.TestCase):
         micro_posix_number_of_seconds, expected_micro_posix_number_of_seconds)
 
     time_elements_object = time_elements.TimeElements()
+
+    micro_posix_number_of_seconds = time_elements_object.GetPlasoTimestamp()
+    self.assertIsNone(micro_posix_number_of_seconds)
+
+
+class TimeElementsInMillisecondsTest(unittest.TestCase):
+  """Tests for the time elements in milliseconds."""
+
+  # pylint: disable=protected-access
+
+  def testInitialize(self):
+    """Tests the initialization function."""
+    time_elements_object = time_elements.TimeElements()
+    self.assertIsNotNone(time_elements_object)
+
+    time_elements_object = time_elements.TimeElements(
+        time_elements_tuple=(2010, 8, 12, 20, 6, 31, 864))
+    self.assertIsNotNone(time_elements_object)
+
+    with self.assertRaises(ValueError):
+      time_elements.TimeElements(
+          time_elements_tuple=(2010, 13, 12, 20, 6, 31))
+
+    with self.assertRaises(ValueError):
+      time_elements.TimeElements(
+          time_elements_tuple=(2010, 13, 12, 20, 6, 31, 1001))
+
+  def testCopyFromString(self):
+    """Tests the CopyFromString function."""
+    time_elements_object = time_elements.TimeElementsInMilliseconds()
+
+    expected_time_elements_tuple = (2010, 8, 12, 0, 0, 0, 0)
+    expected_number_of_seconds = 1281571200
+    time_elements_object.CopyFromString(u'2010-08-12')
+    self.assertEqual(
+        time_elements_object._time_elements_tuple, expected_time_elements_tuple)
+    self.assertEqual(
+        time_elements_object._number_of_seconds, expected_number_of_seconds)
+
+    expected_time_elements_tuple = (2010, 8, 12, 21, 6, 31, 0)
+    expected_number_of_seconds = 1281647191
+    time_elements_object.CopyFromString(u'2010-08-12 21:06:31')
+    self.assertEqual(
+        time_elements_object._time_elements_tuple, expected_time_elements_tuple)
+    self.assertEqual(
+        time_elements_object._number_of_seconds, expected_number_of_seconds)
+
+    expected_time_elements_tuple = (2010, 8, 12, 21, 6, 31, 546)
+    expected_number_of_seconds = 1281647191
+    time_elements_object.CopyFromString(u'2010-08-12 21:06:31.546875')
+    self.assertEqual(
+        time_elements_object._time_elements_tuple, expected_time_elements_tuple)
+    self.assertEqual(
+        time_elements_object._number_of_seconds, expected_number_of_seconds)
+
+    expected_time_elements_tuple = (2010, 8, 12, 22, 6, 31, 546)
+    expected_number_of_seconds = 1281650791
+    time_elements_object.CopyFromString(u'2010-08-12 21:06:31.546875-01:00')
+    self.assertEqual(
+        time_elements_object._time_elements_tuple, expected_time_elements_tuple)
+    self.assertEqual(
+        time_elements_object._number_of_seconds, expected_number_of_seconds)
+
+    expected_time_elements_tuple = (2010, 8, 12, 20, 6, 31, 546)
+    expected_number_of_seconds = 1281643591
+    time_elements_object.CopyFromString(u'2010-08-12 21:06:31.546875+01:00')
+    self.assertEqual(
+        time_elements_object._time_elements_tuple, expected_time_elements_tuple)
+    self.assertEqual(
+        time_elements_object._number_of_seconds, expected_number_of_seconds)
+
+    expected_time_elements_tuple = (1601, 1, 2, 0, 0, 0, 0)
+    expected_number_of_seconds = -11644387200
+    time_elements_object.CopyFromString(u'1601-01-02 00:00:00')
+    self.assertEqual(
+        time_elements_object._time_elements_tuple, expected_time_elements_tuple)
+    self.assertEqual(
+        time_elements_object._number_of_seconds, expected_number_of_seconds)
+
+  def testCopyFromStringISO8601(self):
+    """Tests the CopyFromStringISO8601 function."""
+    time_elements_object = time_elements.TimeElementsInMilliseconds()
+
+    expected_time_elements_tuple = (2010, 8, 12, 0, 0, 0, 0)
+    expected_number_of_seconds = 1281571200
+    time_elements_object.CopyFromStringISO8601(u'2010-08-12')
+    self.assertEqual(
+        time_elements_object._time_elements_tuple, expected_time_elements_tuple)
+    self.assertEqual(
+        time_elements_object._number_of_seconds, expected_number_of_seconds)
+
+    expected_time_elements_tuple = (2010, 8, 12, 21, 6, 31, 0)
+    expected_number_of_seconds = 1281647191
+    time_elements_object.CopyFromStringISO8601(u'2010-08-12T21:06:31')
+    self.assertEqual(
+        time_elements_object._time_elements_tuple, expected_time_elements_tuple)
+    self.assertEqual(
+        time_elements_object._number_of_seconds, expected_number_of_seconds)
+
+    expected_time_elements_tuple = (2010, 8, 12, 21, 6, 31, 0)
+    expected_number_of_seconds = 1281647191
+    time_elements_object.CopyFromStringISO8601(u'2010-08-12T21:06:31Z')
+    self.assertEqual(
+        time_elements_object._time_elements_tuple, expected_time_elements_tuple)
+    self.assertEqual(
+        time_elements_object._number_of_seconds, expected_number_of_seconds)
+
+    expected_time_elements_tuple = (2010, 8, 12, 21, 6, 31, 546)
+    expected_number_of_seconds = 1281647191
+    time_elements_object.CopyFromStringISO8601(u'2010-08-12T21:06:31.546875')
+    self.assertEqual(
+        time_elements_object._time_elements_tuple, expected_time_elements_tuple)
+    self.assertEqual(
+        time_elements_object._number_of_seconds, expected_number_of_seconds)
+
+    expected_time_elements_tuple = (2010, 8, 12, 21, 6, 31, 546)
+    expected_number_of_seconds = 1281647191
+    time_elements_object.CopyFromStringISO8601(u'2010-08-12T21:06:31,546875')
+    self.assertEqual(
+        time_elements_object._time_elements_tuple, expected_time_elements_tuple)
+    self.assertEqual(
+        time_elements_object._number_of_seconds, expected_number_of_seconds)
+
+    expected_time_elements_tuple = (2010, 8, 12, 22, 6, 31, 546)
+    expected_number_of_seconds = 1281650791
+    time_elements_object.CopyFromStringISO8601(
+        u'2010-08-12T21:06:31.546875-01:00')
+    self.assertEqual(
+        time_elements_object._time_elements_tuple, expected_time_elements_tuple)
+    self.assertEqual(
+        time_elements_object._number_of_seconds, expected_number_of_seconds)
+
+    expected_time_elements_tuple = (2010, 8, 12, 20, 6, 31, 546)
+    expected_number_of_seconds = 1281643591
+    time_elements_object.CopyFromStringISO8601(
+        u'2010-08-12T21:06:31.546875+01:00')
+    self.assertEqual(
+        time_elements_object._time_elements_tuple, expected_time_elements_tuple)
+    self.assertEqual(
+        time_elements_object._number_of_seconds, expected_number_of_seconds)
+
+    with self.assertRaises(ValueError):
+      time_elements_object.CopyFromStringISO8601(None)
+
+    with self.assertRaises(ValueError):
+      time_elements_object.CopyFromStringISO8601(
+          u'2010-08-12 21:06:31.546875+01:00')
+
+    # Valid ISO 8601 notations currently not supported.
+    with self.assertRaises(ValueError):
+      time_elements_object.CopyFromStringISO8601(u'2016-W33')
+
+    with self.assertRaises(ValueError):
+      time_elements_object.CopyFromStringISO8601(u'2016-W33-3')
+
+    with self.assertRaises(ValueError):
+      time_elements_object.CopyFromStringISO8601(u'--08-17')
+
+    with self.assertRaises(ValueError):
+      time_elements_object.CopyFromStringISO8601(u'2016-230')
+
+    with self.assertRaises(ValueError):
+      time_elements_object.CopyFromStringISO8601(u'2010-08-12T21:06:31.5')
+
+  def testCopyToStatTimeTuple(self):
+    """Tests the CopyToStatTimeTuple function."""
+    time_elements_object = time_elements.TimeElementsInMilliseconds(
+        time_elements_tuple=(2010, 8, 12, 20, 6, 31, 429))
+
+    expected_stat_time_tuple = (1281643591, 429000)
+    stat_time_tuple = time_elements_object.CopyToStatTimeTuple()
+    self.assertEqual(stat_time_tuple, expected_stat_time_tuple)
+
+    time_elements_object = time_elements.TimeElementsInMilliseconds()
+
+    expected_stat_time_tuple = (None, None)
+    stat_time_tuple = time_elements_object.CopyToStatTimeTuple()
+    self.assertEqual(stat_time_tuple, expected_stat_time_tuple)
+
+  def testGetPlasoTimestamp(self):
+    """Tests the GetPlasoTimestamp function."""
+    time_elements_object = time_elements.TimeElementsInMilliseconds(
+        time_elements_tuple=(2010, 8, 12, 20, 6, 31, 429))
+
+    expected_micro_posix_number_of_seconds = 1281643591429000
+    micro_posix_number_of_seconds = time_elements_object.GetPlasoTimestamp()
+    self.assertEqual(
+        micro_posix_number_of_seconds, expected_micro_posix_number_of_seconds)
+
+    time_elements_object = time_elements.TimeElementsInMilliseconds()
 
     micro_posix_number_of_seconds = time_elements_object.GetPlasoTimestamp()
     self.assertIsNone(micro_posix_number_of_seconds)
