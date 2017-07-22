750cd11aa796be24c824514c870ceeafe579c054
Joachim Metz <joachim.metz@gmail.com>
2017-07-22 14:58:19 +0200

    Code review: 325110043: Changed to setup.py to exclude non-script files from bdist_rpm

--- setup.py
+++ setup.py
@@ -89,8 +89,19 @@ else:
           in_description = True
 
         elif line.startswith('%files'):
-          line = '%files -f INSTALLED_FILES -n {0:s}-%{{name}}'.format(
-              python_package)
+          # Cannot use %{_libdir} here since it can expand to "lib64".
+          lines = [
+              '%files',
+              '%defattr(644,root,root,755)',
+              '%doc ACKNOWLEDGEMENTS AUTHORS LICENSE README',
+              '%{_prefix}/lib/python*/site-packages/dfdatetime/*.py',
+              '%{_prefix}/lib/python*/site-packages/dfdatetime*.egg-info/*',
+              '%exclude %{_prefix}/lib/python*/site-packages/dfdatetime/*.pyc',
+              '%exclude %{_prefix}/lib/python*/site-packages/dfdatetime/*.pyo',
+              '%exclude %{_prefix}/lib/python*/site-packages/dfdatetime/__pycache__/*']
+
+          python_spec_file.extend(lines)
+          break
 
         elif line.startswith('%prep'):
           in_description = False
