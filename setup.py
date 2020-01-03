ad947efbf0162e4011f8ab97657a92c262e4e746
Joachim Metz <joachim.metz@gmail.com>
2020-01-03 13:06:38 +0100

    Updates for Python 2.7 and 3.4 deprecation (#148)

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@
 
 from __future__ import print_function
 
-import locale
 import sys
 
 try:
@@ -23,19 +22,9 @@ except ImportError:
   bdist_rpm = None
 
 version_tuple = (sys.version_info[0], sys.version_info[1])
-if version_tuple[0] not in (2, 3):
-  print('Unsupported Python version: {0:s}.'.format(sys.version))
-  sys.exit(1)
-
-elif version_tuple[0] == 2 and version_tuple < (2, 7):
+if version_tuple < (3, 5):
   print((
-      'Unsupported Python 2 version: {0:s}, version 2.7 or higher '
-      'required.').format(sys.version))
-  sys.exit(1)
-
-elif version_tuple[0] == 3 and version_tuple < (3, 4):
-  print((
-      'Unsupported Python 3 version: {0:s}, version 3.4 or higher '
+      'Unsupported Python version: {0:s}, version 3.5 or higher '
       'required.').format(sys.version))
   sys.exit(1)
 
@@ -78,10 +67,7 @@ else:
       else:
         spec_file = bdist_rpm._make_spec_file(self)
 
-      if sys.version_info[0] < 3:
-        python_package = 'python2'
-      else:
-        python_package = 'python3'
+      python_package = 'python3'
 
       description = []
       requires = ''
@@ -99,9 +85,6 @@ else:
 
         elif line.startswith('Requires: '):
           requires = line[10:]
-          if python_package == 'python3':
-            requires = requires.replace('python-', 'python3-')
-            requires = requires.replace('python2-', 'python3-')
           continue
 
         elif line.startswith('%description'):
@@ -126,22 +109,12 @@ else:
               '%license LICENSE',
               '%doc ACKNOWLEDGEMENTS AUTHORS README']
 
-          if python_package == 'python3':
-            lines.extend([
-                '%{python3_sitelib}/dfdatetime/*.py',
-                '%{python3_sitelib}/dfdatetime*.egg-info/*',
-                '',
-                '%exclude %{_prefix}/share/doc/*',
-                '%exclude %{python3_sitelib}/dfdatetime/__pycache__/*'])
-
-          else:
-            lines.extend([
-                '%{python2_sitelib}/dfdatetime/*.py',
-                '%{python2_sitelib}/dfdatetime*.egg-info/*',
-                '',
-                '%exclude %{_prefix}/share/doc/*',
-                '%exclude %{python2_sitelib}/dfdatetime/*.pyc',
-                '%exclude %{python2_sitelib}/dfdatetime/*.pyo'])
+          lines.extend([
+              '%{python3_sitelib}/dfdatetime/*.py',
+              '%{python3_sitelib}/dfdatetime*.egg-info/*',
+              '',
+              '%exclude %{_prefix}/share/doc/*',
+              '%exclude %{python3_sitelib}/dfdatetime/__pycache__/*'])
 
           python_spec_file.extend(lines)
           break
@@ -151,13 +124,7 @@ else:
 
           python_spec_file.append(
               '%package -n {0:s}-%{{name}}'.format(python_package))
-          if python_package == 'python2':
-            python_spec_file.extend([
-                'Obsoletes: python-dfdatetime < %{version}',
-                'Provides: python-dfdatetime = %{version}'])
-            python_summary = 'Python 2 module of {0:s}'.format(summary)
-          else:
-            python_summary = 'Python 3 module of {0:s}'.format(summary)
+          python_summary = 'Python 3 module of {0:s}'.format(summary)
 
           if requires:
             python_spec_file.append('Requires: {0:s}'.format(requires))
@@ -179,20 +146,6 @@ else:
         python_spec_file.append(line)
 
       return python_spec_file
-
-
-if version_tuple[0] == 2:
-  encoding = sys.stdin.encoding  # pylint: disable=invalid-name
-
-  # Note that sys.stdin.encoding can be None.
-  if not encoding:
-    encoding = locale.getpreferredencoding()
-
-  # Make sure the default encoding is set correctly otherwise on Python 2
-  # setup.py sdist will fail to include filenames with Unicode characters.
-  reload(sys)  # pylint: disable=undefined-variable
-
-  sys.setdefaultencoding(encoding)  # pylint: disable=no-member
 
 
 dfdatetime_description = (
